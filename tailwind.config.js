73807d91394cc8a314e5655c5e9b2a67c5c5daf7
Sergio Camilo Ochoa Rodriguez <zhertsh@MacBook-Pro-de-Sergio.local>
2025-02-11 22:17:31 -0500

    module in tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ["./src/**/*.{html,js,jsx,ts,tsx}"], // Ajusta esto según tu estructura
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+    purge: false,
+  };
